39039ea3ed99087ce04fe09ec1a4b1afb9769b81
Anthony Dang <anthonyp.dang@yahoo.com>
2014-11-25 11:43:51 -0700

    Personal Site

---
+++ index.php
@@ -0,0 +1,311 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Project Portfolio</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" type="text/css" href="homepage/bootstrap/css/bootstrap.min.css" />
+
+    <!-- Fonts -->
+    <link rel="stylesheet" type="text/css" href="homepage/font-awesome/css/font-awesome.min.css" />
+
+	<link href="homepage/css/animate.css" rel="stylesheet" />
+    <!-- Squad theme CSS -->
+    <link href="homepage/css/style.css" rel="stylesheet">
+	<link href="homepage/css/default.css" rel="stylesheet">
+	
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+	<!-- Preloader -->
+	<div id="preloader">
+	  <div id="load"></div>
+	</div>
+
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top"></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#aboutme">About Me</a>
+                    </li>
+					<li>
+                        <a class="page-scroll" href="#coursework">Coursework</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+	
+
+	<!-- Section: intro -->
+    <section id="intro" class="intro" style="background:url('images/background_image.jpg') no-repeat top center;">
+	
+		<div class="slogan">
+			<h2>Anthony Dang's</h2>
+			<h2>Portfolio</h2>
+		</div>
+		
+		<div class="page-scroll">
+			<a href="#aboutme" class="btn btn-circle">
+				<i class="fa fa-angle-double-down animated"></i>
+			</a>
+		</div>
+    </section>
+	<!-- /Section: intro -->
+	<!-- Section: about me -->
+    <section id="aboutme">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">About Me</h2>
+                    <h3 class="section-subheading text-muted"></h3>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-4">
+					<img src="images/education.png" alt="Education">
+					<h4 class="service-heading">Education</h4>
+                    <p class="text-muted">Currently attending Arizona State University pursuing a Bachelors of Science in Computer Information System & Supply Chain Management. Currently set to graduate December 2014.</p>
+                </div>
+                <div class="col-md-4">
+					<img src="images/career.png" alt="Education">
+                    <h4 class="service-heading">Work Experience</h4>
+                    <p class="text-muted">United States Marine Corps, NYSE, UTC Aerospace Systems </p>
+                </div>
+				<div class="col-md-4">
+					<img src="images/future.png" alt="Education">
+                    <h4 class="service-heading">Objective</h4>
+                    <p class="text-muted">To obtain a challenging position with career growth potential specializing in Computer Information Systems and/or Supply Chain Management.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+		<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<section id="coursework">
+			 <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Coursework</h2>
+                    <h3 class="section-subheading text-muted">Below are some coursework that are directly related to my career.</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-sm-6 portfolio-item">
+                   <img src="images/statistics.png" alt="statistics">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                            </div>
+                        </div>
+                    <div class="portfolio-caption">
+                        <h4>Statistics</h4>
+						<p>Basic concepts and methods of statistics, including descriptive statistics, significance tests, estimation, sampling, and correlation.</p>
+                    </div>
+                </div>
+			<div class="col-md-6 col-sm-6 portfolio-item">
+                   <img src="images/computerliteracy.png" alt="computerliteracy">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                            </div>
+                        </div>
+                    <div class="portfolio-caption">
+                        <h4>Computer Literacy</h4>
+						<p>Basic concepts of computer information systems from a business intelligence perspective and the uses of application software with emphasis on database and spreadsheet packages for efficient and effective problem solving.</p>
+                    </div>
+                </div>
+			<div class="col-md-6 col-sm-6 portfolio-item">
+                   <img src="images/interpersonal.png" alt="interpersonal">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                            </div>
+                        </div>
+                    <div class="portfolio-caption">
+                        <h4>Interpersonal Communication</h4>
+						<p>Verbal and nonverbal communication in platform speaking. Discussion and practice in vocal and physical delivery and in purposeful organization and development of public communication.</p>
+                    </div>
+                </div>	
+		
+				<div class="col-md-6 col-sm-6 portfolio-item">
+                   <img src="images/GlobalSupply.png" alt="Global Supply Operations">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                            </div>
+                        </div>
+                    <div class="portfolio-caption">
+                        <h4>Global Supply Operations</h4>
+						<p>Resources and information to create and deliver products globally. Interfirm systems and industry supply chains. Customer, producer, and employee perspectives.</p>
+                    </div>
+                </div>	
+			<div class="col-md-6 col-sm-6 portfolio-item">
+                   <img src="images/BusinessAna.png" alt="Business Analysis">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                            </div>
+                        </div>
+                    <div class="portfolio-caption">
+                        <h4>Business Analysis</h4>
+						<p> Basic concept of the overview of analytics in the business context; concepts of strategic and operational analytics; overview of concepts like dimensional modeling, the Model Lifecycle, data mining, big data, KPIs and metrics, ERP and analytics, in-database/memory analytics; real-time analytics and data stream analysis.</p>
+                    </div>
+                </div>	
+			<div class="col-md-6 col-sm-6 portfolio-item">
+                   <img src="images/Enterprise.png" alt="Networks and Distributed Systems">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                            </div>
+                        </div>
+                    <div class="portfolio-caption">
+                        <h4>Networks and Distributed Systems</h4>
+						<p> Basic concept of advanced topics such as communications protocols, distributed systems, network security and client-server systems; applications based on platforms such as.NET and networked UNIX.</p>
+                    </div>
+                </div>
+			<div class="col-md-6 col-sm-6 portfolio-item">
+                   <img src="images/BusinessDatabase.png" alt="Business Database Concepts">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                            </div>
+                        </div>
+                    <div class="portfolio-caption">
+                        <h4>Networks and Distributed Systems</h4>
+						<p> Basic concept of Database theory, design, and application, including the entity-relationship model; the relational, hierarchical, and network database models; and query languages</p>
+                    </div>
+                </div>
+				
+            </div>
+        </div>
+	
+	</section>
+	<!-- /Section: about me -->
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	
+	 <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Contact me</h2>
+                    <h3 class="section-subheading text-muted">Please feel free to contact me.</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <form name="sentMessage" id="contactForm" novalidate>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Your Name *" id="name" required data-validation-required-message="Please enter your name.">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control" placeholder="Your Email *" id="email" required data-validation-required-message="Please enter your email address.">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="form-group">
+                                    <input type="tel" class="form-control" placeholder="Your Phone *" id="phone" required data-validation-required-message="Please enter your phone number.">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <textarea class="form-control" placeholder="Your Message *" id="message" required data-validation-required-message="Please enter a message."></textarea>
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="col-lg-12 text-center">
+                                <div id="success"></div>
+                                <button type="submit" class="btn btn-xl">Send Message</button>
+                            </div>
+                        </div>
+                    </form>
+					<br>
+					<br>
+					<br>
+					<br>
+                </div>
+            </div>
+        </div>
+    </section>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-lg-12">
+					<div class="wow shake" data-wow-delay="0.4s">
+					<div class="page-scroll marginbot-30">
+						<a href="#intro" id="totop" class="btn btn-circle">
+							<i class="fa fa-angle-double-up animated"></i>
+						</a>
+					</div>
+					</div>
+					<p>&copy;Copyright 2014 - Anthony Dang. All rights reserved.</p>
+				</div>
+			</div>	
+		</div>
+	</footer>
+	
+    <!-- Core JavaScript Files -->
+    <script src="js/jquery.min.js"></script>    
+    <script src="js/jquery.easing.min.js"></script>	
+	<script src="js/jquery.scrollTo.js"></script>
+	<script src="js/wow.min.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="js/custom.js"></script>
+	
+	
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+</body>
+
+</html>
